f0ad13e702780933ba16a08cea62862821679fa1
bernardodelbosque <bernardodelbosque@hotmail.com>
2017-02-09 21:29:13 -0600

    second day of tutorial

--- src/app/hero.js
+++ src/app/hero.js
@@ -1,10 +1,13 @@
 "use strict";
 var Hero = (function () {
-    function Hero(id, name) {
-        this.id = id;
-        this.name = name;
+    function Hero() {
     }
     return Hero;
 }());
 exports.Hero = Hero;
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/ 
 //# sourceMappingURL=hero.js.map
